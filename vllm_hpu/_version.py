4691f5bcfbbd0972c0f85907fbe462e8f451f94b
Konrad Zawora <kzawora@habana.ai>
2025-07-09 12:24:19 +0200

    Add HPU CI tests (#9)
    * Add HPU CI tests
    
    Signed-off-by: Konrad Zawora <kzawora@habana.ai>
    
    * oopsie wrong definition
    
    Signed-off-by: Konrad Zawora <kzawora@habana.ai>
    
    ---------
    
    Signed-off-by: Konrad Zawora <kzawora@habana.ai>

--- vllm_hpu/_version.py
+++ vllm_hpu/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev30+gc191c84.d20250612'
-__version_tuple__ = version_tuple = (0, 1, 'dev30', 'gc191c84.d20250612')
+__version__ = version = '0.1.dev8+g83f99c5.d20250708'
+__version_tuple__ = version_tuple = (0, 1, 'dev8', 'g83f99c5.d20250708')
