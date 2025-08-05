ae9c3ea1dd78f963830c48b12bfc4dec3c7e56da
Kacper Pietkun <kacper.pietkun00@gmail.com>
2025-08-05 09:17:39 +0200

    Change PT_HPU_LAZY_MODE default value (#58)
    Signed-off-by: Kacper Pietkun <kpietkun@habana.ai>
    Signed-off-by: Thomas Atta-fosu <tattafosu@habana.ai>

--- vllm_gaudi/platform.py
+++ vllm_gaudi/platform.py
@@ -143,7 +143,7 @@ class HpuPlatform(Platform):
         # Eager backend (PT_HPU_LAZY_MODE = 0) must be selected for
         # torch.compile support
         os.environ['PT_HPU_WEIGHT_SHARING'] = '0'
-        is_lazy = os.environ.get('PT_HPU_LAZY_MODE', '1') == '1'
+        is_lazy = os.environ.get('PT_HPU_LAZY_MODE', '0') == '1'
         if is_lazy:
             torch._dynamo.config.disable = True
             # NOTE multi-HPU inference with HPUGraphs (lazy-only)
