f2dd9a4e05feae7da0bdfdc55e4e74914b2c179c
Tianmu Li <tianmu.li@intel.com>
2025-08-14 00:23:33 +0300

    Add missing tests
    Signed-off-by: Tianmu Li <tianmu.li@intel.com>

--- tests/full_tests/ci_tests.sh
+++ tests/full_tests/ci_tests.sh
@@ -42,6 +42,7 @@ echo "Test with deepseek_v2 + inc dynamic quantization + tp 2 successful"
 # structured output
 echo "Testing structured output"
 echo HABANA_VISIBLE_DEVICES=all VLLM_CONTIGUOUS_PA=False VLLM_SKIP_WARMUP=True PT_HPU_LAZY_MODE=1 VLLM_USE_V1=1 python -u vllm-gaudi/tests/full_tests/structured_outputs.py 
+HABANA_VISIBLE_DEVICES=all VLLM_CONTIGUOUS_PA=False VLLM_SKIP_WARMUP=True PT_HPU_LAZY_MODE=1 VLLM_USE_V1=1 python -u vllm-gaudi/tests/full_tests/structured_outputs.py 
 if [ $? -ne 0 ]; then
     echo "Error: Test failed for structured outputs" >&2
     exit -1
