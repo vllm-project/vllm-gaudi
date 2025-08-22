acf91601ff7376ed8c5cff2efa82c069c4008a8f
Andrzej Kotłowski <akotlowski@habana.ai>
2025-08-22 11:10:24 +0300

    Enable dynamic_shapes_compilation by default
    Signed-off-by: Andrzej Kotłowski <akotlowski@habana.ai>

--- vllm_gaudi/extension/features.py
+++ vllm_gaudi/extension/features.py
@@ -72,7 +72,7 @@ def get_features():
         Value('linear_bucketing', True),
         Value('bucketing_strategy', FirstEnabled(*bucketing_strategies), env_var_type=choice(*bucketing_strategies)),
         Value('regional_compilation', True, env_var='VLLM_T_COMPILE_REGIONAL_COMPILATION', env_var_type=boolean),
-        Value('dynamic_shapes_compilation', False, env_var='VLLM_T_COMPILE_DYNAMIC_SHAPES', env_var_type=boolean),
+        Value('dynamic_shapes_compilation', True, env_var='VLLM_T_COMPILE_DYNAMIC_SHAPES', env_var_type=boolean),
         Value('fullgraph_compilation', False, env_var='VLLM_T_COMPILE_FULLGRAPH', env_var_type=boolean),
     ]
     return split_values_and_flags(features)
