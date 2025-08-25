2f08872af20c4ac0a362ab3c8613373c9d36165d
Agata Dobrzyniewicz <adobrzyniewicz@habana.ai>
2025-08-25 12:24:20 +0300

    Test fix
    Signed-off-by: Agata Dobrzyniewicz <adobrzyniewicz@habana.ai>

--- tests/unit_tests/worker/test_hpu_model_runner.py
+++ tests/unit_tests/worker/test_hpu_model_runner.py
@@ -147,7 +147,6 @@ def _schedule_new_request(*req_ids: str) -> SchedulerOutput:
         scheduled_encoder_inputs={},
         num_common_prefix_blocks=0,
         finished_req_ids=set(),
-        free_encoder_input_ids=[],
         structured_output_request_ids={},
         grammar_bitmask=None,
     )
@@ -213,7 +212,6 @@ def test_update_states_request_finished(model_runner, dist_init):
         scheduled_encoder_inputs={},
         num_common_prefix_blocks=0,
         finished_req_ids={req_id},
-        free_encoder_input_ids=[],
         structured_output_request_ids={},
         grammar_bitmask=None,
     )
@@ -245,7 +243,6 @@ def test_update_states_request_resumed(model_runner, dist_init):
         scheduled_encoder_inputs={},
         num_common_prefix_blocks=0,
         finished_req_ids=set(),
-        free_encoder_input_ids=[],
         structured_output_request_ids={},
         grammar_bitmask=None,
     )
@@ -272,7 +269,6 @@ def test_update_states_request_resumed(model_runner, dist_init):
         scheduled_encoder_inputs={},
         num_common_prefix_blocks=0,
         finished_req_ids=set(),
-        free_encoder_input_ids=[],
         structured_output_request_ids={},
         grammar_bitmask=None,
     )
@@ -353,7 +349,6 @@ def test_update_states_no_changes(model_runner, dist_init):
         scheduled_encoder_inputs={},
         num_common_prefix_blocks=0,
         finished_req_ids=set(),
-        free_encoder_input_ids=[],
         structured_output_request_ids={},
         grammar_bitmask=None,
     )
@@ -390,7 +385,6 @@ def test_update_states_request_unscheduled(model_runner, dist_init):
         scheduled_encoder_inputs={},
         num_common_prefix_blocks=0,
         finished_req_ids=set(),
-        free_encoder_input_ids=[],
         structured_output_request_ids={},
         grammar_bitmask=None,
     )
