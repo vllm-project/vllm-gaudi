f86e707548c1a6496d8c5dd0f0f5b082465de4df
Agata Dobrzyniewicz <adobrzyniewicz@habana.ai>
2025-08-26 09:37:38 +0300

    Fix test args
    Signed-off-by: Agata Dobrzyniewicz <adobrzyniewicz@habana.ai>

--- tests/unit_tests/test_bucketing.py
+++ tests/unit_tests/test_bucketing.py
@@ -63,7 +63,10 @@ def test_generate_decode_buckets():
     bs_bucket_config = [1, 32, 128]
     blocks_bucket_config = [128, 128, 2048]
     max_blocks = 1024
+    max_model_len = 131072
+    block_size=128
     buckets = linear.generate_decode_buckets(bs_bucket_config,
-                                             blocks_bucket_config, max_blocks)
+                                             blocks_bucket_config, max_blocks,
+                                             max_model_len, block_size)
     assert len(buckets) == 72
     assert all(blocks <= max_blocks for _, _, blocks in buckets)
