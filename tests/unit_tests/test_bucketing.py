5085b687829eedadacf2aaf90cbd66ea1717144d
Agata Dobrzyniewicz <adobrzyniewicz@habana.ai>
2025-09-02 14:27:31 +0300

    Precommit for tests
    Signed-off-by: Agata Dobrzyniewicz <adobrzyniewicz@habana.ai>

--- tests/unit_tests/test_bucketing.py
+++ tests/unit_tests/test_bucketing.py
@@ -6,7 +6,6 @@
 ###############################################################################
 
 import pytest
-from unittest.mock import MagicMock
 
 import vllm_gaudi.extension.bucketing.linear as linear
 from vllm_gaudi.extension.bucketing.common import generate_buckets
@@ -53,12 +52,11 @@ def test_generate_prompt_buckets():
     max_model_len = 2048
     bs = 16
     prompt_bs = 16
-    prefix_caching = True
     bs_range = [1, 2, 4, 8, 16]
     query_range = [512, 1024]
     ctx_range = [0, 1, 2, 3, 4]
     buckets = generate_buckets(bs_range, query_range, ctx_range, True,
-                               max_model_len, bs, prompt_bs, 
+                               max_model_len, bs, prompt_bs,
                                max_num_batched_tokens, block_size)
     assert len(buckets) == 25
     assert all(bs * seq <= max_num_batched_tokens for bs, seq, _ in buckets)
@@ -71,9 +69,13 @@ def test_generate_decode_buckets():
     bs = 64
     prompt_bs = 64
     bs_range = [1, 2, 4, 8, 16, 32, 64]
-    blocks_range = [128, 256, 384, 512, 640, 768, 896, 1024, 1152, 1280, 1408, 1536, 1664, 1792, 1920, 2048]
+    blocks_range = [
+        128, 256, 384, 512, 640, 768, 896, 1024, 1152, 1280, 1408, 1536, 1664,
+        1792, 1920, 2048
+    ]
     buckets = generate_buckets(bs_range, [1, 1, 1], blocks_range, False,
                                max_model_len, bs, prompt_bs,
                                max_num_batched_tokens, block_size)
     assert len(buckets) == 15
-    assert all(ctx <= bs * (max_model_len // block_size) for bs, _, ctx in buckets)
+    assert all(ctx <= bs * (max_model_len // block_size)
+               for bs, _, ctx in buckets)
