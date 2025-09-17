f4f3d4d59f281b057907eb44d23090b1d1ddfa00
PatrykWo <patryk.wolsza@intel.com>
2025-09-17 17:06:34 +0300

    Compose update path to a benchmark
    Signed-off-by: PatrykWo <patryk.wolsza@intel.com>

--- .cd/templates/template_vllm_benchmark.sh
+++ .cd/templates/template_vllm_benchmark.sh
@@ -21,7 +21,7 @@ if [[ "$DATASET_NAME" == "hf" ]]; then
 fi
 
 ## Start benchmarking vLLM serving
-python3 /workspace/vllm/benchmarks/benchmark_serving.py \
+python3 /workspace/vllm-project/benchmarks/benchmark_serving.py \
                 --model $MODEL \
                 --base-url http://localhost:8000 \
                 --endpoint $ENDPOINT \
