0a40c23b9dba78b89a976c1c4f1b7d563a56117b
Agata Dobrzyniewicz <adobrzyniewicz@habana.ai>
2025-09-19 14:21:20 +0300

    After review
    Signed-off-by: Agata Dobrzyniewicz <adobrzyniewicz@habana.ai>

--- vllm_gaudi/extension/bucketing/unified.py
+++ vllm_gaudi/extension/bucketing/unified.py
@@ -13,12 +13,12 @@ from vllm_gaudi.extension.runtime import get_config
 class UnifiedBucketingStrategy():
 
     def get_unified_cfgs(self, bs, max_model_len, block_size, max_blocks, max_num_batched_tokens):
-        # [min, step, max, turning_point]
-        query_cfg = [128, 128, max_num_batched_tokens, bs]
+        # [min, max, turning_point]
+        query_cfg = [block_size, max_num_batched_tokens, bs]
         max_shared_ctx = math.ceil(max_model_len // block_size) * bs
-        shared_ctx_cfg = [0, 1, max_shared_ctx, bs]
-        max_unique_ctx = max_blocks  # // 2 - 500 # TODO: OOM
-        unique_ctx_cfg = [0, 1, max_unique_ctx, bs]
+        shared_ctx_cfg = [0, max_shared_ctx, bs]
+        max_unique_ctx = max_blocks 
+        unique_ctx_cfg = [0, max_unique_ctx, bs]
         return query_cfg, shared_ctx_cfg, unique_ctx_cfg
 
     def get_range(self, cfg):
@@ -26,42 +26,8 @@ class UnifiedBucketingStrategy():
         return sorted(range_for_cfg)
 
 
-def exponential_range(number_of_values, minimum_value, rounding_value, maximum_value):
-    values_range = []
-    if number_of_values == 1:
-        return [maximum_value]
-    number_of_exponential_values = number_of_values
-    for i in range(number_of_values):
-        power_unpadded = minimum_value * np.float_power(maximum_value / minimum_value,
-                                                        (1. / float(number_of_exponential_values - 1)) * i)
-        new_value = math.ceil(power_unpadded / rounding_value) * rounding_value
-        values_range.append(new_value)
-    return values_range
-
-
 def warmup_unified_range(cfg):
-    '''
-    [min, step, max, limit, turning_point]
-        bs
-           ↑
-           |                          max   \
-           |                          .      |
-           |                         .       |
-           |                        .        |
-           |                       .         | - exponantial with base1
-           |                     .           |
-           |                  .              |
-           |              .                  |
-           |          .                     /
-           |         X - turning point               
-           |         .                      \
-           |        .                        |
-           |      .                          | - exponential with base0
-           |    .                            |
-           | min                            /
-           +-------------------------------------------→ 
-    '''
-    bmin, bstep, bmax, turning_point = cfg
+    bmin, bmax, turning_point = cfg
 
     buckets: Set[Tuple[int, int]] = set()
 
