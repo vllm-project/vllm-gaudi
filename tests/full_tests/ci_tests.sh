3e9cb1bfe7dd87fd20d4e0d8904d18d42b18b12e
Konrad Zawora <kzawora@habana.ai>
2025-09-22 15:48:54 +0300

    revert more test changes
    Signed-off-by: Konrad Zawora <kzawora@habana.ai>

--- tests/full_tests/ci_tests.sh
+++ tests/full_tests/ci_tests.sh
@@ -19,26 +19,25 @@ fi
 echo "Test with tensor parallel size 2 passed"
 
 # mla and moe
-# NOTE(adobrzyn): upstream bug
-#echo "Testing MLA and MoE with vllm-hpu plugin v1"
-#echo HABANA_VISIBLE_DEVICES=all VLLM_SKIP_WARMUP=true PT_HPU_LAZY_MODE=1 VLLM_USE_V1=1 python -u vllm-gaudi/tests/full_tests/generate.py --model deepseek-ai/DeepSeek-V2-Lite-Chat --trust-remote-code
-#HABANA_VISIBLE_DEVICES=all VLLM_SKIP_WARMUP=true PT_HPU_LAZY_MODE=1 VLLM_USE_V1=1 python -u vllm-gaudi/tests/full_tests/generate.py --model deepseek-ai/DeepSeek-V2-Lite-Chat --trust-remote-code
-#if [ $? -ne 0 ]; then
-#    echo "Error: Test failed for deepseek v2 lite passed" >&2
-#    exit -1
-#fi
-#echo "Test with deepseek v2 lite passed"
+echo "Testing MLA and MoE with vllm-hpu plugin v1"
+echo HABANA_VISIBLE_DEVICES=all VLLM_SKIP_WARMUP=true PT_HPU_LAZY_MODE=1 VLLM_USE_V1=1 python -u vllm-gaudi/tests/full_tests/generate.py --model deepseek-ai/DeepSeek-V2-Lite-Chat --trust-remote-code
+HABANA_VISIBLE_DEVICES=all VLLM_SKIP_WARMUP=true PT_HPU_LAZY_MODE=1 VLLM_USE_V1=1 python -u vllm-gaudi/tests/full_tests/generate.py --model deepseek-ai/DeepSeek-V2-Lite-Chat --trust-remote-code
+if [ $? -ne 0 ]; then
+    echo "Error: Test failed for deepseek v2 lite passed" >&2
+    exit -1
+fi
+echo "Test with deepseek v2 lite passed"
 
 # deepseek v2 + inc + dynamic quantization + tp2
-#echo "Testing deepseek_v2 + inc dynamic quantization + tp2"
-#echo QUANT_CONFIG=vllm-gaudi/tests/models/language/generation/inc_dynamic_quant.json HABANA_VISIBLE_DEVICES=all VLLM_SKIP_WARMUP=true PT_HPU_LAZY_MODE=1 VLLM_USE_V1=1 python -u vllm-gaudi/tests/full_tests/generate.py --model deepseek-ai/DeepSeek-V2-Lite-Chat --trust-remote-code  --quantization inc --kv_cache_dtype fp8_inc
-#QUANT_CONFIG=vllm-gaudi/tests/models/language/generation/inc_dynamic_quant.json \
-#HABANA_VISIBLE_DEVICES=all VLLM_SKIP_WARMUP=true PT_HPU_LAZY_MODE=1 VLLM_USE_V1=1 python -u vllm-gaudi/tests/full_tests/generate.py --model deepseek-ai/DeepSeek-V2-Lite-Chat --trust-remote-code --quantization inc --tensor-parallel-size 2
-#if [ $? -ne 0 ]; then
-#    echo "Error: Test failed for deepseek_v2 + inc dynamic quantization + tp2" >&2
-#    exit -1
-#fi
-#echo "Test with deepseek_v2 + inc dynamic quantization + tp 2 successful"
+echo "Testing deepseek_v2 + inc dynamic quantization + tp2"
+echo QUANT_CONFIG=vllm-gaudi/tests/models/language/generation/inc_dynamic_quant.json HABANA_VISIBLE_DEVICES=all VLLM_SKIP_WARMUP=true PT_HPU_LAZY_MODE=1 VLLM_USE_V1=1 python -u vllm-gaudi/tests/full_tests/generate.py --model deepseek-ai/DeepSeek-V2-Lite-Chat --trust-remote-code  --quantization inc --kv_cache_dtype fp8_inc
+QUANT_CONFIG=vllm-gaudi/tests/models/language/generation/inc_dynamic_quant.json \
+HABANA_VISIBLE_DEVICES=all VLLM_SKIP_WARMUP=true PT_HPU_LAZY_MODE=1 VLLM_USE_V1=1 python -u vllm-gaudi/tests/full_tests/generate.py --model deepseek-ai/DeepSeek-V2-Lite-Chat --trust-remote-code --quantization inc --tensor-parallel-size 2
+if [ $? -ne 0 ]; then
+    echo "Error: Test failed for deepseek_v2 + inc dynamic quantization + tp2" >&2
+    exit -1
+fi
+echo "Test with deepseek_v2 + inc dynamic quantization + tp 2 successful"
 
 # structured output
 echo "Testing structured output"
