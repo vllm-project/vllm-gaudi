e3ead692c17f0564934edc5ee41d0bf8d7c9b2ae
slokesha <slokeshappa@habana.ai>
2025-09-30 00:48:14 +0000

    fixed hang
    Signed-off-by: slokesha <slokeshappa@habana.ai>

--- tests/unit_tests/test_embedding.py
+++ tests/unit_tests/test_embedding.py
@@ -4,6 +4,9 @@
 # This source code is licensed under the Apache 2.0 license found in the
 # LICENSE file in the root directory of this source tree.
 ###############################################################################
+import pytest
+from argparse import Namespace
+import vllm
 
 PROMPTS = [
     "Hello, my name is",
@@ -12,25 +15,24 @@ PROMPTS = [
     "The future of AI is",
 ]
 
-# @pytest.mark.xfail(reason="skip, expect fix by SW-241408")
-# @pytest.mark.parametrize(
-#     "model",
-#     [
-#         "intfloat/e5-mistral-7b-instruct",
-#         # "ssmits/Qwen2-7B-Instruct-embed-base",
-#         # "BAAI/bge-multilingual-gemma2",
-#     ])
-# def test_embeddings(model):
+@pytest.mark.parametrize(
+    "model",
+    [
+        "intfloat/e5-mistral-7b-instruct",
+        # "ssmits/Qwen2-7B-Instruct-embed-base",
+        # "BAAI/bge-multilingual-gemma2",
+    ])
+def test_embeddings(model):
 
-#     args = Namespace(model=model, runner="pooling", enforce_eager=True)
-#     llm = vllm.LLM(**vars(args))
+    args = Namespace(model=model, runner="pooling", enforce_eager=True)
+    llm = vllm.LLM(**vars(args))
 
-#     outputs = llm.embed(PROMPTS)
+    outputs = llm.embed(PROMPTS)
 
-#     assert isinstance(outputs, list)
-#     assert len(outputs) == len(PROMPTS)
+    assert isinstance(outputs, list)
+    assert len(outputs) == len(PROMPTS)
 
-#     for out in outputs:
-#         emb = out.outputs.embedding
-#         assert isinstance(emb, list)
-#         assert all(isinstance(x, float) for x in emb)
+    for out in outputs:
+        emb = out.outputs.embedding
+        assert isinstance(emb, list)
+        assert all(isinstance(x, float) for x in emb)
