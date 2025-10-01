9c346800674966e5796efa9c2ad107bac2d7a728
Spurthi Lokeshappa <slokeshappa@habana.ai>
2025-09-30 18:29:02 -0700

    Fix Embeding hang (#291)
    Signed-off-by: slokesha <slokeshappa@habana.ai>
    Co-authored-by: Chendi.Xue <chendi.xue@intel.com>

--- tests/unit_tests/test_embedding.py
+++ tests/unit_tests/test_embedding.py
@@ -4,6 +4,9 @@
 # This source code is licensed under the Apache 2.0 license found in the
 # LICENSE file in the root directory of this source tree.
 ###############################################################################
+import pytest
+from argparse import Namespace
+import vllm
 
 import pytest
 from argparse import Namespace
