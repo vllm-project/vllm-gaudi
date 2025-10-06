069e195771f60626e03029b0c0a781858243e976
Konrad Zawora <kzawora@habana.ai>
2025-10-06 19:05:46 +0300

    Add custom HPU block queue
    Signed-off-by: Konrad Zawora <kzawora@habana.ai>

---
+++ vllm_gaudi/sched/hpu_block_pool.py
@@ -0,0 +1,44 @@
+from vllm.v1.core.block_pool import BlockPool, BlockHashToBlockMap
+from vllm_gaudi.extension.logger import logger as init_logger
+from vllm.v1.core.kv_cache_utils import (
+    KVCacheBlock, )
+from vllm.distributed.kv_events import (
+    KVCacheEvent, )
+
+logger = init_logger()
+
+
+class HPUBlockPool(BlockPool):
+
+    def __init__(
+        self,
+        num_gpu_blocks: int,
+        enable_caching: bool,
+        enable_kv_cache_events: bool = False,
+    ):
+        msg = f"Using {type(self).__name__}"
+        logger.info(msg)
+        assert isinstance(num_gpu_blocks, int) and num_gpu_blocks > 0
+        self.num_gpu_blocks = num_gpu_blocks
+        self.enable_caching = enable_caching
+        # All kv-cache blocks.
+        self.blocks: list[KVCacheBlock] = [KVCacheBlock(idx) for idx in range(num_gpu_blocks)]
+        # Free block queue that constructs and manipulates a doubly linked
+        # list of free blocks (including eviction candidates when caching is
+        # enabled).
+        from vllm.v1.core.kv_cache_utils import FreeKVCacheBlockQueue
+        self.free_block_queue = FreeKVCacheBlockQueue(self.blocks)
+
+        #        self.free_block_queue = HPUFreeKVCacheBlockQueue(self.blocks)
+
+        # Cache for block lookup
+        self.cached_block_hash_to_block: BlockHashToBlockMap = BlockHashToBlockMap()
+
+        # To represent a placeholder block with block_id=0.
+        # The ref_cnt of null_block is not maintained, needs special care to
+        # avoid freeing it.
+        self.null_block = self.free_block_queue.popleft()
+        self.null_block.is_null = True
+
+        self.enable_kv_cache_events = enable_kv_cache_events
+        self.kv_event_queue: list[KVCacheEvent] = []
--- vllm_gaudi/sched/hpu_scheduler.py
+++ vllm_gaudi/sched/hpu_scheduler.py
@@ -4,6 +4,12 @@
 from __future__ import annotations
 
 from vllm.v1.core.sched.scheduler import Scheduler
+from vllm.config import VllmConfig
+from vllm.v1.kv_cache_interface import KVCacheConfig
+from vllm.v1.structured_output import StructuredOutputManager
+from vllm.multimodal import MULTIMODAL_REGISTRY, MultiModalRegistry
+from vllm_gaudi.sched.hpu_kv_cache_manager import HPUKVCacheManager
+
 from vllm_gaudi.extension.logger import logger as init_logger
 
 logger = init_logger()
@@ -11,7 +17,32 @@ logger = init_logger()
 
 class HPUScheduler(Scheduler):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(
+        self,
+        vllm_config: VllmConfig,
+        kv_cache_config: KVCacheConfig,
+        structured_output_manager: StructuredOutputManager,
+        mm_registry: MultiModalRegistry = MULTIMODAL_REGISTRY,
+        include_finished_set: bool = False,
+        log_stats: bool = False,
+    ) -> None:
         msg = f"Using {type(self).__name__}"
         logger.info(msg)
-        super().__init__(*args, **kwargs)
+        super().__init__(
+            vllm_config=vllm_config,
+            kv_cache_config=kv_cache_config,
+            structured_output_manager=structured_output_manager,
+            mm_registry=mm_registry,
+            include_finished_set=include_finished_set,
+            log_stats=log_stats,
+        )
+        #        return
+        self.kv_cache_manager = HPUKVCacheManager(
+            kv_cache_config=kv_cache_config,
+            max_model_len=self.max_model_len,
+            enable_caching=self.cache_config.enable_prefix_caching,
+            use_eagle=self.use_eagle,
+            log_stats=self.log_stats,
+            enable_kv_cache_events=self.enable_kv_cache_events,
+            dcp_world_size=self.dcp_world_size,
+        )
