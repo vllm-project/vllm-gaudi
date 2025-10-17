6c45b6ae4f78d7d78d61cf4c6db23df7dfaa49a8
Jimin Ha <jimin.ha@intel.com>
2025-10-17 09:03:36 -0700

    Fix enforce_eager/torch_compile mode error
    Signed-off-by: Jimin Ha <jimin.ha@intel.com>

--- vllm_gaudi/models/gemma3_mm.py
+++ vllm_gaudi/models/gemma3_mm.py
@@ -19,39 +19,30 @@ class HpuGemma3ForConditionalGeneration(Gemma3ForConditionalGeneration):
         pixel_values = image_input["pixel_values"]
         num_patches = image_input["num_patches"]
 
-        batch_breakdown = self.vision_bucket_manager.greedy_plan(pixel_values.shape[0],
-                                                                 self.vision_bucket_manager.multimodal_buckets)
-        start_idx = 0
-        image_embeds_multibatches = []
+        if hasattr(self, 'vision_bucket_manager'):
+            batch_breakdown = self.vision_bucket_manager.greedy_plan(pixel_values.shape[0],
+                                                                     self.vision_bucket_manager.multimodal_buckets)
+            start_idx = 0
+            image_embeds_multibatches = []
 
-        for i in batch_breakdown:
-            end_idx = start_idx + i
-            indices = torch.arange(start_idx, end_idx)
-            batch_sliced_pixel_values = torch.index_select(pixel_values, dim=0, index=indices)
+            for i in batch_breakdown:
+                end_idx = start_idx + i
+                indices = torch.arange(start_idx, end_idx).to(pixel_values.device)
+                batch_sliced_pixel_values = torch.index_select(pixel_values, dim=0, index=indices)
 
+                image_features = self._image_pixels_to_features(
+                    self.vision_tower,
+                    batch_sliced_pixel_values,
+                )
+                image_embeds = self.multi_modal_projector(image_features)
+                image_embeds_multibatches += [image_embeds.clone()]
+                start_idx = end_idx
+            image_embeds = torch.cat(image_embeds_multibatches, dim=0)
+        else:
             image_features = self._image_pixels_to_features(
                 self.vision_tower,
-                batch_sliced_pixel_values,
+                pixel_values,
             )
             image_embeds = self.multi_modal_projector(image_features)
-            image_embeds_multibatches += [image_embeds.clone()]
-            start_idx = end_idx
-        image_embeds = torch.cat(image_embeds_multibatches, dim=0)
 
         return [e.flatten(0, 1) for e in image_embeds.split(num_patches.tolist())]
-
-    def greedy_plan(self, batchsize, available_batchsizes):
-        # sort descending
-        available_batchsizes_sorted = sorted(available_batchsizes, key=lambda x: -x)
-        idx = 0
-        left_to_process = batchsize
-        result = []
-        while (left_to_process > 0 and idx < len(available_batchsizes_sorted)):
-            if available_batchsizes_sorted[idx] <= left_to_process:
-                result += [available_batchsizes_sorted[idx]]
-                left_to_process -= available_batchsizes_sorted[idx]
-            else:
-                idx += 1
-        if left_to_process > 0:
-            result += [available_batchsizes_sorted[-1]]  # this will be padded
-        return result
