1666a9b1f45010cdcbf3a0ba3a0bf73280a252d7
Chendi Xue <chendi.xue@intel.com>
2025-10-18 02:24:12 +0000

    [NIXL]Enable prefill TP < Decode TP with host_buffer
    Signed-off-by: Chendi Xue <chendi.xue@intel.com>

--- vllm_gaudi/platform.py
+++ vllm_gaudi/platform.py
@@ -217,6 +217,8 @@ class HpuPlatform(Platform):
         """Copy blocks from src_cache to dst_cache on HPU."""
         if isinstance(dst_cache, tuple):
             _src_cache = src_cache[:, src_block_indices]
+            if _src_cache.shape[2:] != dst_cache.shape[2:]:
+                _src_cache = _src_cache.permute(0, 1, 3, 2, 4)
             for i in range(len(dst_cache)):
                 dst_cache[i].index_copy_(0, dst_block_indices, _src_cache[i].to(dst_cache[i].device))
         else:
@@ -234,6 +236,9 @@ class HpuPlatform(Platform):
         """Copy blocks from HPU to host (CPU)."""
         if isinstance(src_cache, tuple):
             _src_cache = torch.stack([c[src_block_indices] for c in src_cache], dim=0)
+            # permute back to original shape
+            if _src_cache.shape[2:] != dst_cache.shape[2:]:
+                _src_cache = _src_cache.permute(0, 1, 3, 2, 4)
             dst_cache[:, dst_block_indices] = _src_cache.cpu()
         else:
             dst_cache[dst_block_indices] = src_cache[src_block_indices].cpu()
