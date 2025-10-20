a4170a69450c4ba05b6bc219a8b92102dad4d3eb
Agata Dobrzyniewicz <adobrzyniewicz@habana.ai>
2025-10-20 15:19:41 +0300

    Default vals optimization
    Signed-off-by: Agata Dobrzyniewicz <adobrzyniewicz@habana.ai>

--- vllm_gaudi/extension/bucketing/linear.py
+++ vllm_gaudi/extension/bucketing/linear.py
@@ -15,12 +15,12 @@ class LinearBucketingStrategy:
         use_merged_prefill = get_config().merged_prefill
         prefix_caching = get_config().prefix_caching
 
-        prompt_bs_bucket_cfg = read_bucket_settings('prompt', 'bs', min=1, step=32, max=max_num_prefill_seqs)
+        prompt_bs_bucket_cfg = read_bucket_settings('prompt', 'bs', min=1, step=1, max=max_num_prefill_seqs)
         prompt_query_bucket_cfg = read_bucket_settings('prompt',
                                                        'seq',
                                                        min=block_size,
                                                        step=block_size,
-                                                       max=max_model_len)
+                                                       max=max_num_batched_tokens)
         max_ctx = math.ceil((max_model_len - prompt_query_bucket_cfg[0]) // block_size)
         prompt_ctx_bucket_cfg = read_bucket_settings('prompt', 'ctx', min=0, step=1, max=max_ctx)
 
@@ -55,9 +55,13 @@ class LinearBucketingStrategy:
 
     def get_decode_cfgs(self, max_num_seqs, block_size, max_num_batched_tokens, max_model_len, max_blocks):
         prefix_caching = get_config().prefix_caching
+        contiguous_pa = get_config().use_contiguous_pa
 
         decode_bs_bucket_cfg = read_bucket_settings('decode', 'bs', min=1, step=32, max=max_num_seqs)
         decode_query_bucket_cfg = [1, 1, 1]
+        max_decode_blocks = max(math.ceil(max_model_len * max_num_seqs // block_size), block_size)
+        if contiguous_pa:
+            max_decode_blocks = max_blocks
         decode_block_bucket_cfg = read_bucket_settings('decode', 'block', min=1, step=block_size, max=max_blocks)
         if decode_block_bucket_cfg[2] > max_blocks:
             logger().info(
