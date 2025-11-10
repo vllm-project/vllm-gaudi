61e47be88193167f264184bfb9b63ef93cbce8c3
Soila Kavulya <soila.p.kavulya@intel.com>
2025-11-10 14:47:13 -0800

    [SW-242523] Support per-tensor FP8 scaling (#483)
    Support FP8 inference for models with per-tensor scales
    
    ---------
    
    Signed-off-by: Kavulya, Soila P <soila.p.kavulya@intel.com>

--- vllm_gaudi/extension/scales.py
+++ vllm_gaudi/extension/scales.py
@@ -8,17 +8,7 @@ import torch
 import habana_frameworks.torch.core as htcore
 import habana_frameworks.torch.utils.experimental as htexp
 
-try:
-    from vllm.platforms import current_platform
-
-    def is_hpu_gaudi2():
-        return current_platform.is_hpu() and htexp._get_device_type() == htexp.synDeviceType.synDeviceGaudi2
-except ImportError:
-
-    def is_hpu_gaudi2():
-        import habana_frameworks.torch.utils.experimental as htexp
-        device_type = htexp._get_device_type()
-        return device_type == htexp.synDeviceType.synDeviceGaudi2
+is_hpu_gaudi2 = htexp._get_device_type() == htexp.synDeviceType.synDeviceGaudi2
 
 
 def get_hpu_gaudi2_scale_factor():
@@ -120,7 +110,7 @@ class ScaleToPow2:
 class ConvertScaleToHwAligned:
 
     def __init__(self, device_type="GAUDI3"):
-        self.device_type = "GAUDI2" if is_hpu_gaudi2() else "GAUDI3"
+        self.device_type = "GAUDI2" if is_hpu_gaudi2 else "GAUDI3"
 
     def calc(self, scale):
         if self.device_type == "GAUDI2":
