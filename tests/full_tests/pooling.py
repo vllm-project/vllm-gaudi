182a45c40f2efbb1178c4afaee1f3f7327f70e79
Spurthi Lokeshappa <slokeshappa@habana.ai>
2025-11-20 22:11:19 +0530

    WarmUp for Pooling - Embed Task (#170)
    Enabled Warmup for Pooling
    
    ---------
    
    Signed-off-by: slokesha <slokeshappa@habana.ai>
    Signed-off-by: Agata Dobrzyniewicz <adobrzyniewicz@habana.ai>
    Signed-off-by: attafosu <thomas.atta-fosu@intel.com>
    Signed-off-by: Chendi Xue <Chendi.Xue@intel.com>
    Signed-off-by: Tianmu Li <tianmu.li@intel.com>
    Signed-off-by: Xinyu Chen <xinyu1.chen@intel.com>
    Signed-off-by: PatrykWo <patryk.wolsza@intel.com>
    Signed-off-by: Wuxun Zhang <wuxun.zhang@intel.com>
    Signed-off-by: Kamil Kaczor <kamil.kaczor@intel.com>
    Signed-off-by: Michal Adamczyk <michal.adamczyk@intel.com>
    Signed-off-by: Kavulya, Soila P <soila.p.kavulya@intel.com>
    Signed-off-by: Konrad Zawora <kzawora@habana.ai>
    Signed-off-by: Jimin Ha <jimin.ha@intel.com>
    Signed-off-by: Mohit Deopujari <mdeopujari@habana.ai>
    Signed-off-by: Harish Subramony <hsubramony@habana.ai>
    Signed-off-by: Karol Damaszke <kdamaszke@habana.ai>
    Signed-off-by: Vivek Goel <vivek.goel@intel.com>
    Signed-off-by: Iryna Boiko <iboiko@habana.ai>
    Signed-off-by: Spurthi Lokeshappa <slokeshappa@habana.ai>
    Co-authored-by: Agata Dobrzyniewicz <160237065+adobrzyn@users.noreply.github.com>
    Co-authored-by: Chendi.Xue <chendi.xue@intel.com>
    Co-authored-by: Konrad Zawora <kzawora@habana.ai>
    Co-authored-by: Thomas Atta-Fosu <thomas.atta-fosu@intel.com>
    Co-authored-by: Tianmu Li <tianmu.li@intel.com>
    Co-authored-by: Xinyu Chen <xinyu1.chen@intel.com>
    Co-authored-by: PatW <patryk.wolsza@intel.com>
    Co-authored-by: Wuxun Zhang <wuxun.zhang@intel.com>
    Co-authored-by: Kamil Kaczor <kamil.kaczor@intel.com>
    Co-authored-by: Michal Adamczyk <madamczyk@habana.ai>
    Co-authored-by: Soila Kavulya <soila.p.kavulya@intel.com>
    Co-authored-by: Jimin Ha <jimin.ha@intel.com>
    Co-authored-by: Mohit Deopujari <mdeopujari@habana.ai>
    Co-authored-by: Harish Subramony <hsubramony@habana.ai>
    Co-authored-by: Karol Damaszke <kdamaszke@habana.ai>
    Co-authored-by: Vivek Goel <vivek.goel@intel.com>
    Co-authored-by: Iryna Boiko <iboiko@habana.ai>

--- tests/full_tests/pooling.py
+++ tests/full_tests/pooling.py
@@ -22,7 +22,7 @@ def create_parser():
     parser.set_defaults(
         model="intfloat/e5-mistral-7b-instruct",
         runner="pooling",
-        enforce_eager=True,
+        enforce_eager=False,
     )
     return parser
 
