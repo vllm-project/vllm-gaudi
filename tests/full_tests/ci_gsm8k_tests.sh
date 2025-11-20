e71e2fae693278788c4c36900b5492b0c5a9a897
Iryna Boiko <iboiko@habana.ai>
2025-11-20 12:58:39 +0200

    hourly tests: run_spec_decode_ngram_test enabled, run_gemma3_test disabled
    Signed-off-by: Iryna Boiko <iboiko@habana.ai>

--- tests/full_tests/ci_gsm8k_tests.sh
+++ tests/full_tests/ci_gsm8k_tests.sh
@@ -12,12 +12,13 @@ echo $VLLM_GAUDI_PREFIX
 
 # Gemma3 with image input
 run_gemma3_test() {
-    echo "➡️ Testing gemma-3-4b-it..."
-    VLLM_SKIP_WARMUP=true PT_HPU_LAZY_MODE=1 python -u "${VLLM_GAUDI_PREFIX}/tests/models/language/generation/generation_mm.py" --model-card-path "${VLLM_GAUDI_PREFIX}/tests/full_tests/model_cards/gemma-3-4b-it.yaml"
-    echo "✅ Test with multimodal-support with gemma-3-4b-it passed."
-    echo "➡️ Testing gemma-3-4b-it with multiple images(applying sliding_window)..."
-    VLLM_SKIP_WARMUP=true PT_HPU_LAZY_MODE=1 python -u "${VLLM_GAUDI_PREFIX}/tests/models/language/generation/generation_mm_multi.py" --model-card-path "${VLLM_GAUDI_PREFIX}/tests/full_tests/model_cards/gemma-3-27b-it.yaml"
-    echo "✅ Test with multimodal-support with multiple images gemma-3-27b-it passed."
+    #echo "➡️ Testing gemma-3-4b-it..."
+    #VLLM_SKIP_WARMUP=true PT_HPU_LAZY_MODE=1 python -u "${VLLM_GAUDI_PREFIX}/tests/models/language/generation/generation_mm.py" --model-card-path "${VLLM_GAUDI_PREFIX}/tests/full_tests/model_cards/gemma-3-4b-it.yaml"
+    #echo "✅ Test with multimodal-support with gemma-3-4b-it passed."
+    #echo "➡️ Testing gemma-3-4b-it with multiple images(applying sliding_window)..."
+    #VLLM_SKIP_WARMUP=true PT_HPU_LAZY_MODE=1 python -u "${VLLM_GAUDI_PREFIX}/tests/models/language/generation/generation_mm_multi.py" --model-card-path "${VLLM_GAUDI_PREFIX}/tests/full_tests/model_cards/gemma-3-27b-it.yaml"
+    #echo "✅ Test with multimodal-support with multiple images gemma-3-27b-it passed."
+    # Test cases are commented because of PR27772
 }
 
 # Basic model test
@@ -212,8 +213,7 @@ run_qwen2_5_vl_test() {
 # Spec decode with ngram
 run_spec_decode_ngram_test() {
     echo "➡️ Testing Spec-decode with ngram..."
-    #VLLM_CONTIGUOUS_PA=False VLLM_SKIP_WARMUP=True PT_HPU_LAZY_MODE=1 python "${VLLM_GAUDI_PREFIX}/tests/full_tests/spec_decode.py" --task ngram --assert_acc_rate 0.25 --osl 1024
-    # Test case is skipped because of PR27922
+    VLLM_CONTIGUOUS_PA=False VLLM_SKIP_WARMUP=True PT_HPU_LAZY_MODE=1 python "${VLLM_GAUDI_PREFIX}/tests/full_tests/spec_decode.py" --task ngram --assert_acc_rate 0.25 --osl 1024
     echo "✅ Test with spec decode with ngram passed."
 }
 
