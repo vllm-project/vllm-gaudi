0e087987357e81310c0f2eede2acd7ac3c9a9537
Tianmu Li <tianmu.li@intel.com>
2025-11-20 23:57:58 -0800

    Add async scheduling for unified attention (#414)
    #134 + #184 + #360 for unified attention
    
    ---------
    
    Signed-off-by: Tianmu Li <tianmu.li@intel.com>

--- vllm_gaudi/v1/worker/hpu_model_runner.py
+++ vllm_gaudi/v1/worker/hpu_model_runner.py
@@ -1451,6 +1451,26 @@ class HPUModelRunner(KVConnectorModelRunnerMixin):
         return bool(req_id in self.input_batch.req_type and \
             self.input_batch.req_type[req_id] == "decode")
 
+    def _get_num_decodes(self) -> int:
+        num_reqs = self.input_batch.num_reqs
+        assert num_reqs > 0
+        #TODO: remove later
+
+        num_decodes = 0
+        for i in range(num_reqs):
+            req_id = self.input_batch.req_ids[i]
+            assert req_id is not None
+
+            num_computed_tokens = self.input_batch.num_computed_tokens_cpu[i]
+            num_prompt_tokens = self.input_batch.num_prompt_tokens[i]
+
+            if num_computed_tokens < num_prompt_tokens and \
+                not self.is_decoder_only(req_id):
+                # This is prompt
+                continue
+            num_decodes += 1
+        return num_decodes
+
     def _get_prompts_and_decodes(
         self,
         scheduler_output: "SchedulerOutput",
@@ -2831,10 +2851,17 @@ class HPUModelRunner(KVConnectorModelRunnerMixin):
         block_table = self.input_batch.block_table[0].get_cpu_tensor()[:num_reqs, :max_blocks].clone().to(torch.int64)
         if self.defragmenter.enabled:
             block_table.apply_(self.defragmenter.resolve)
+        input_ids_hpu = None
+        num_decodes = 0
+        if self.use_async_scheduling:
+            num_decodes = self._get_num_decodes()
+            self._prepare_input_ids(scheduler_output)
+            input_ids_hpu = self.input_ids_hpu
 
         return create_unified_batch(self.input_batch.req_ids, all_token_ids, num_computed_tokens, num_scheduled_tokens,
                                     num_prompt_tokens, block_table, self.block_size, self.dtype,
-                                    self.unified_attn_persistent_ctx, self.unified_bucketing_fn, self.get_dp_padding)
+                                    self.unified_attn_persistent_ctx, self.unified_bucketing_fn, self.get_dp_padding,
+                                    input_ids_hpu, num_decodes)
 
     @torch.inference_mode()
     def unified_execute_model(self,
@@ -2869,23 +2896,40 @@ class HPUModelRunner(KVConnectorModelRunnerMixin):
             sampling_metadata = self._prepare_sampling(batch_changed, selected_req_ids, pad_to=logits_device.shape[0])
             sampler_output = self.sampler(logits=logits_device, sampling_metadata=sampling_metadata)
 
+        # Copy some objects so they don't get modified after returning.
+        # This is important when using async scheduling.
+        req_ids_output_copy = self.input_batch.req_ids.copy()
+        req_id_to_index_output_copy = \
+            self.input_batch.req_id_to_index.copy()
+
         with self.profiler.record_event('internal', 'unified_postprocess'):
-            sampled_token_ids_cpu = sampler_output.sampled_token_ids.cpu()
-
-            sampled_token_ids_np = sampled_token_ids_cpu.numpy()
             sampled_token_ids: list[np.ndarray] = [np.array([], dtype=np.int32) for _ in batch.req_ids_cpu]
-            for req_id, tokens_array in zip(selected_req_ids, sampled_token_ids_np):
-                idx = self.input_batch.req_id_to_index[req_id]
-                sampled_token_ids[idx] = tokens_array
+            if self.use_async_scheduling:
+                sampled_token_ids_hpu = sampler_output.sampled_token_ids.view(-1, 1)
+                self.input_batch.prev_sampled_token_ids = sampled_token_ids_hpu.flatten()
+                invalid_req_indices = batch.invalid_req_indices
+                invalid_req_indices_set = set(invalid_req_indices)
+                self.input_batch.prev_sampled_token_ids_invalid_indices = invalid_req_indices_set
+                self.input_batch.prev_req_id_to_index = {
+                    req_id: i
+                    for i, req_id in enumerate(self.input_batch.req_ids) if i not in invalid_req_indices_set
+                }
+            else:
+                sampled_token_ids_cpu = sampler_output.sampled_token_ids.cpu()
 
-            #TODO: add support for multi-token output
-            assert sampled_token_ids_cpu.size(1) == 1, 'Currently only single token output is supported!'
-            sampled_token_ids_cpu = sampled_token_ids_cpu.flatten()
-            htorch.core.mark_step()
+                sampled_token_ids_np = sampled_token_ids_cpu.numpy()
+                for req_id, tokens_array in zip(selected_req_ids, sampled_token_ids_np):
+                    idx = self.input_batch.req_id_to_index[req_id]
+                    sampled_token_ids[idx] = tokens_array
 
-            sampled_token_ids_cpu = sampled_token_ids_cpu.index_select(0, batch.logits_groups_cpu)
-            self.input_batch.token_ids_cpu_tensor.index_put_((batch.logits_groups_cpu, batch.new_token_positions_cpu),
-                                                             sampled_token_ids_cpu)
+                #TODO: add support for multi-token output
+                assert sampled_token_ids_cpu.size(1) == 1, 'Currently only single token output is supported!'
+                sampled_token_ids_cpu = sampled_token_ids_cpu.flatten()
+                htorch.core.mark_step()
+
+                sampled_token_ids_cpu = sampled_token_ids_cpu.index_select(0, batch.logits_groups_cpu)
+                self.input_batch.token_ids_cpu_tensor.index_put_(
+                    (batch.logits_groups_cpu, batch.new_token_positions_cpu), sampled_token_ids_cpu)
 
             ######### UPDATE REQUEST STATE WITH GENERATED TOKENS #########
             num_reqs = len(selected_req_ids)
@@ -2898,6 +2942,22 @@ class HPUModelRunner(KVConnectorModelRunnerMixin):
                 self.input_batch.token_ids_cpu[i, seq_len:seq_len + num_tokens] = token_ids
                 self.input_batch.num_tokens[i] += len(token_ids)
                 req_state.output_token_ids.extend(token_ids.tolist())
+
+        if self.use_async_scheduling:
+            model_runner_output = ModelRunnerOutput(
+                req_ids=req_ids_output_copy,
+                req_id_to_index=req_id_to_index_output_copy,
+                sampled_token_ids=sampled_token_ids,
+                logprobs=None,
+                prompt_logprobs_dict={},
+                pooler_output=[],
+            )
+            return AsyncHPUModelRunnerOutput(
+                model_runner_output=model_runner_output,
+                sampled_token_ids=sampled_token_ids_hpu,
+                invalid_req_indices=invalid_req_indices,
+                async_output_copy_stream=self.async_output_copy_stream,
+            )
 
         model_runner_output = ModelRunnerOutput(
             req_ids=batch.req_ids_cpu,
@@ -3112,15 +3172,17 @@ class HPUModelRunner(KVConnectorModelRunnerMixin):
                         # When there are multiple requests in the batch (e.g. self.use_merged_prefill=True),
                         # the last token position is the sum of all prompt lengths - 1
                         # This logic also holds when there is only one request in the batch
-                        logits_indices_append = torch.tensor([torch.sum(prompt_len) - 1],
-                                                             device=token_ids.device,
-                                                             dtype=torch.int32)
+                        logits_indices_append = torch.full((1, ),
+                                                           torch.sum(prompt_len) - 1,
+                                                           device=logits_indices.device,
+                                                           dtype=logits_indices.dtype)
                         logits_indices = torch.cat([logits_indices, logits_indices_append])
                     if self.use_async_scheduling:
                         # Discard partial prefill logit for async scheduling
                         # Depends on 1 decode token/batch
                         prefill_start_idx = num_decodes
                         invalid_req_indices.append(prefill_start_idx + idx)
+
                 htorch.core.mark_step()
                 non_flattened_hidden_states, aux_hidden_states, \
                     sample_hidden_states, logits_device = \
