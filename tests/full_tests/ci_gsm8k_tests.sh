5ae2b9f5ec70eefc823181a968b33747da101340
Chen Haifeng <haifeng.chen@intel.com>
2025-11-24 11:22:43 +0800

    Add the CI test case for num_speculative_tokens > 1
    Signed-off-by: Chen Haifeng <haifeng.chen@intel.com>

--- tests/full_tests/ci_gsm8k_tests.sh
+++ tests/full_tests/ci_gsm8k_tests.sh
@@ -232,6 +232,13 @@ run_spec_decode_eagle3_test() {
     echo "✅ Test with spec decode with eagle3 passed."
 }
 
+# Spec decode with eagle3 and num_speculative_tokens = 2
+run_spec_decode_eagle3_num_spec_2_test() {
+    echo "➡️ Testing Spec-decode with eagle3 and num_speculative_tokens = 2..."
+    VLLM_CONTIGUOUS_PA=False VLLM_SKIP_WARMUP=True PT_HPU_LAZY_MODE=1 python "${VLLM_GAUDI_PREFIX}/tests/full_tests/spec_decode.py" --task eagle3 --assert_acc_rate 0.58 --osl 2048 --num_spec_tokens 2
+    echo "✅ Test with spec decode with eagle3 and num_speculative_tokens = 2 passed."
+}
+
 # NOTE(Chendi): Failed due upstream, expect fix by SW-241408
 # Embedding-model-support for v1
 run_embedding_model_test() {
