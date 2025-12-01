628e9ccdad20d6d32e016a609fbcdb91aa93250f
PatrykWo <patryk.wolsza@intel.com>
2025-12-01 11:21:33 +0100

    Revert "initial test"
    This reverts commit 64b6373053273f86cbbd2e302b5d1b12c3b4af56.

--- .cd/templates/template_vllm_server.sh
+++ .cd/templates/template_vllm_server.sh
@@ -15,7 +15,7 @@ vllm serve $MODEL \
         --block-size $BLOCK_SIZE \
         --dtype $DTYPE \
         --tensor-parallel-size $TENSOR_PARALLEL_SIZE \
-        --download_dir $MODEL \
+        --download_dir $HF_HOME \
         --max-model-len $MAX_MODEL_LEN \
         --gpu-memory-utilization $GPU_MEM_UTILIZATION \
         --max-num-seqs $MAX_NUM_SEQS \
