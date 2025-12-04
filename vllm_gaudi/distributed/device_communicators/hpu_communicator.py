ad8b9cc59d5760c791c33d48572968a1c72aab9c
Xinyu Chen <xinyu1.chen@intel.com>
2025-12-04 14:03:18 +0800

    DP: dispatch tensor in FusedMoEMethod
    Signed-off-by: Xinyu Chen <xinyu1.chen@intel.com>

--- vllm_gaudi/distributed/device_communicators/hpu_communicator.py
+++ vllm_gaudi/distributed/device_communicators/hpu_communicator.py
@@ -64,46 +64,8 @@ class HpuCommunicator(DeviceCommunicatorBase):
                  hidden_states: torch.Tensor,
                  router_logits: torch.Tensor,
                  is_sequence_parallel: bool = False) -> tuple[torch.Tensor, torch.Tensor]:
-        assert self.dp_group is not None
-        assert hidden_states.dim() == 2, "Input hidden states must be 2D"
-
-        dp_metadata = get_hpu_dp_metadata()
-        if dp_metadata is not None:
-            hidden_states_across_dp = dp_metadata.hidden_states_across_dp
-            router_logits_across_dp = dp_metadata.router_logits_across_dp
-        else:
-            # create hidden_states_across_dp tensor
-            input_size = hidden_states.size()
-            # Allocate output tensor.
-            output_size = list(input_size)
-            if is_sequence_parallel:
-                # if sequence parallel enabled, hidden states was already being chunked by sp_size
-                output_size[0] *= self.world_size
-            else:
-                output_size[0] *= self.dp_world_size
-            hidden_states_across_dp = torch.empty(output_size, dtype=hidden_states.dtype, device=hidden_states.device)
-
-            # create router_logits_across_dp tensor
-            router_logits_size = router_logits.size()
-            router_logits_output_size = list(router_logits_size)
-            if is_sequence_parallel:
-                router_logits_output_size[0] *= self.world_size
-            else:
-                router_logits_output_size[0] *= self.dp_world_size
-            router_logits_across_dp = torch.empty(router_logits_output_size,
-                                                  dtype=router_logits.dtype,
-                                                  device=router_logits.device)
-
-        torch.distributed.all_gather_into_tensor(
-            hidden_states_across_dp,
-            hidden_states,
-            group=get_ep_group().device_group if is_sequence_parallel else self.dp_group.device_group)
-
-        torch.distributed.all_gather_into_tensor(
-            router_logits_across_dp,
-            router_logits,
-            group=get_ep_group().device_group if is_sequence_parallel else self.dp_group.device_group)
-        return hidden_states_across_dp, router_logits_across_dp
+        # Use dispatch_tensor in the plugin FusedMoEMethod for better performance
+        return hidden_states, router_logits
 
     def combine(self, hidden_states: torch.Tensor, is_sequence_parallel: bool = False) -> torch.Tensor:
         if htorch.utils.internal.is_lazy():
