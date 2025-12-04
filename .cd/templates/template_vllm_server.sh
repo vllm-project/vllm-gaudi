4dc4582aaeb38c07d3d5d7cfa1f3ff015a5ea037
PatrykWo <patryk.wolsza@intel.com>
2025-12-04 18:40:21 +0200

    Add command printout for vLLM server startup
    Signed-off-by: PatrykWo <patryk.wolsza@intel.com>

--- .cd/templates/template_vllm_server.sh
+++ .cd/templates/template_vllm_server.sh
@@ -10,6 +10,16 @@ if [ $ASYNC_SCHEDULING -gt 0 ]; then # Checks if using async scheduling
     EXTRA_ARGS+=" --async_scheduling"
 fi
 
+## Executing command print
+
+printf "\nStarting vLLM server with the following command:\n"
+printf "\nvllm serve $MODEL --block-size $BLOCK_SIZE --dtype $DTYPE \
+--tensor-parallel-size $TENSOR_PARALLEL_SIZE --download_dir $HF_HOME \
+--max-model-len $MAX_MODEL_LEN --gpu-memory-utilization $GPU_MEM_UTILIZATION \
+--max-num-seqs $MAX_NUM_SEQS --generation-config vllm \
+--max_num_batched_tokens $MAX_NUM_BATCHED_TOKENS \
+--disable-log-requests $EXTRA_ARGS \n\n"
+
 ## Start server
 vllm serve $MODEL \
         --block-size $BLOCK_SIZE \
