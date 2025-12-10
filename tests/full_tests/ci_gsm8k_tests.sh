f029c1d285b09d2f47634e728e40dd45a013bf0b
Spurthi Lokeshappa <spurthi.lokeshappa@intel.com>
2025-12-10 19:28:06 +0530

    Update tests/full_tests/ci_gsm8k_tests.sh
    Co-authored-by: Paweł Olejniczak <pawelolejniczak92@gmail.com>
    Signed-off-by: Spurthi Lokeshappa <spurthi.lokeshappa@intel.com>

--- tests/full_tests/ci_gsm8k_tests.sh
+++ tests/full_tests/ci_gsm8k_tests.sh
@@ -272,7 +272,7 @@ run_UA_spec_decode_eagle3_test() {
     echo "✅ Test with spec decode with eagle3 passed."
 }
 
-Embedding-model-support for v1
+# Embedding-model-support for v1
 run_embedding_model_test() {
    echo "➡️ Testing Embedding-model-support for v1..."
    HABANA_VISIBLE_DEVICES=all VLLM_CONTIGUOUS_PA=False VLLM_SKIP_WARMUP=false PT_HPU_LAZY_MODE=1 python -u "${VLLM_GAUDI_PREFIX}/tests/full_tests/pooling.py" --model intfloat/e5-mistral-7b-instruct --trust-remote-code
