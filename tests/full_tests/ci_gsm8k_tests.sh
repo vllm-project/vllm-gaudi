e6d1cc87707d7e759635786337ceefcb08d6f76f
Spurthi Lokeshappa <spurthi.lokeshappa@intel.com>
2025-12-11 20:18:30 +0530

    Handle "pooling_states" for Embed Task (#699)
    Signed-off-by: slokesha <slokeshappa@habana.ai>
    Signed-off-by: Spurthi Lokeshappa <spurthi.lokeshappa@intel.com>
    Co-authored-by: Paweł Olejniczak <pawelolejniczak92@gmail.com>

--- tests/full_tests/ci_gsm8k_tests.sh
+++ tests/full_tests/ci_gsm8k_tests.sh
@@ -272,12 +272,11 @@ run_UA_spec_decode_eagle3_test() {
     echo "✅ Test with spec decode with eagle3 passed."
 }
 
-# NOTE(Chendi): Failed due upstream, expect fix by SW-241408
 # Embedding-model-support for v1
 run_embedding_model_test() {
-    echo "➡️ Testing Embedding-model-support for v1..."
-    HABANA_VISIBLE_DEVICES=all VLLM_CONTIGUOUS_PA=False VLLM_SKIP_WARMUP=false PT_HPU_LAZY_MODE=1 python -u "${VLLM_GAUDI_PREFIX}/tests/full_tests/pooling.py" --model intfloat/e5-mistral-7b-instruct --trust-remote-code
-    echo "✅ Embedding-model-support for v1 successful."
+   echo "➡️ Testing Embedding-model-support for v1..."
+   HABANA_VISIBLE_DEVICES=all VLLM_CONTIGUOUS_PA=False VLLM_SKIP_WARMUP=false PT_HPU_LAZY_MODE=1 python -u "${VLLM_GAUDI_PREFIX}/tests/full_tests/pooling.py" --model intfloat/e5-mistral-7b-instruct --trust-remote-code
+   echo "✅ Embedding-model-support for v1 successful."
 }
 
 # pd_disaggregate_nixl_libfabric
