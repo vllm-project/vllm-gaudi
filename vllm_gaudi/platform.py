3f0414de86450671ac91b5efd6479b2ef7b00363
Konrad Zawora <kzawora@habana.ai>
2025-12-15 18:42:15 +0200

    Intial Unified MLA enablement
    Signed-off-by: Konrad Zawora <kzawora@habana.ai>

--- vllm_gaudi/platform.py
+++ vllm_gaudi/platform.py
@@ -55,15 +55,20 @@ class HpuPlatform(Platform):
     ) -> str:
         if use_sparse:
             raise NotImplementedError("Sparse Attention is not supported on HPU.")
-        if use_mla:
-            logger.info("Using HPUAttentionMLA backend.")
-            return ("vllm_gaudi.attention.backends.hpu_attn."
-                    "HPUMLAAttentionBackend")
         elif get_config().unified_attn:
+            if use_mla:
+                logger.info("Using HPUUnifiedMLA backend.")
+                return ("vllm_gaudi.attention.backends.hpu_attn."
+                        "HPUUnifiedMLABackend")
             logger.info("Using UnifiedAttention backend.")
             return ("vllm_gaudi.attention.backends."
                     "hpu_attn.HPUUnifiedAttentionBackend")
         else:
+            if use_mla:
+                logger.info("Using HPUAttentionMLA backend.")
+                return ("vllm_gaudi.attention.backends.hpu_attn."
+                        "HPUMLAAttentionBackend")
+
             logger.info("Using HPUAttentionV1 backend.")
             return ("vllm_gaudi.v1.attention.backends."
                     "hpu_attn.HPUAttentionBackendV1")
