15e513b51c91168e720da08ec45c6b9ed551d46c
lvkaokao <kaokao.lv@intel.com>
2025-12-17 15:03:16 +0000

    apply hardware aligned scale function.
    Signed-off-by: lvkaokao <kaokao.lv@intel.com>

--- vllm_gaudi/extension/ops.py
+++ vllm_gaudi/extension/ops.py
@@ -12,6 +12,7 @@ import math
 import habana_frameworks.torch.core as htcore
 from vllm_gaudi.extension.runtime import get_config
 from vllm_gaudi.extension.utils import get_kv_fetch_extra_args
+from vllm_gaudi.extension.scales import ConvertScaleToHwAligned
 
 import habana_frameworks.torch.utils.experimental as htexp
 import types
@@ -1186,6 +1187,8 @@ def requantize_with_max_scale(weight: torch.Tensor, weight_scale: torch.Tensor,
                               logical_widths: list[int]) -> tuple[torch.Tensor, torch.Tensor]:
     # Max scale to be used for requanitzation.
     max_w_scale = weight_scale.max()
+    # hw aligned
+    max_w_scale = ConvertScaleToHwAligned().calc(max_w_scale)
     # QKV / MLP is fused in the on disk checkpoint if any of the
     # weight scales are still set to the default since we initialize
     # N weight scales for N shards but we only load 1 weight scale
