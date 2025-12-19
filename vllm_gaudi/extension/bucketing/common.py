0e10d870d47acb41c26e6b03b74c5b7618b4c13e
Katarzyna Fojcik <kfojcik@habana.ai>
2025-12-19 11:59:15 +0200

    fix
    Signed-off-by: Katarzyna Fojcik <kfojcik@habana.ai>

--- vllm_gaudi/extension/bucketing/common.py
+++ vllm_gaudi/extension/bucketing/common.py
@@ -441,13 +441,12 @@ def generate_buckets(bs_range,
             if all(bucket_filter(bs, query, blocks) for bucket_filter in filters):
                 buckets.add(corrector(bs, query, blocks))
     else:
-        max_ctx = max(ctx for _, ctx in buckets_2d)
         for bs_idx, bs in enumerate(bs_range):
             for ctx_idx, ctx in enumerate(ctx_range):
                 local_buckets = expand_to_neighbor_buckets(bs_idx, bs_range, ctx_idx, ctx_range,
                                                            max_num_batched_tokens) if not is_prompt else {(bs, ctx)}
                 buckets_2d.update(local_buckets)
-
+        max_ctx = max(ctx for _, ctx in buckets_2d)
         for bs, ctx in buckets_2d:
             if is_prompt and ctx == max_ctx:
                 # skip biggest ctx only in prompt mode; we will add it via get_max_bucket_per_query later
