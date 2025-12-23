a870ba32caeb3c3fc2d8f5810a442ac1fc28e376
Iryna Boiko <iboiko@habana.ai>
2025-12-23 12:21:29 +0200

    Fix for PR31036, part3
    Signed-off-by: Iryna Boiko <iboiko@habana.ai>

--- tests/full_tests/ci_gsm8k_tests.sh
+++ tests/full_tests/ci_gsm8k_tests.sh
@@ -12,6 +12,7 @@ echo $VLLM_GAUDI_PREFIX
 
 # Gemma3 with image input
 run_gemma3_test() {
+    
     echo "➡️ Testing gemma-3-4b-it..."
     VLLM_SKIP_WARMUP=true PT_HPU_LAZY_MODE=1 python -u "${VLLM_GAUDI_PREFIX}/tests/models/language/generation/generation_mm.py" --model-card-path "${VLLM_GAUDI_PREFIX}/tests/full_tests/model_cards/gemma-3-4b-it.yaml"
     echo "✅ Test with multimodal-support with gemma-3-4b-it passed."
