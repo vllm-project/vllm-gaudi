9d8e2723f370874f40bd8e99944f98af379d76b1
Libin Tang <libin.tang@intel.com>
2025-12-23 14:11:41 -0800

    Pick model runner change related to PR30475. Also overwrite qwen3_vl function to use _merge_multimodal_embeddings with index copy.

--- vllm_gaudi/models/__init__.py
+++ vllm_gaudi/models/__init__.py
@@ -1,6 +1,5 @@
 from vllm.model_executor.models.registry import ModelRegistry
 
-
 def register_model():
     from vllm_gaudi.models.gemma3_mm import HpuGemma3ForConditionalGeneration  # noqa: F401
 
@@ -11,3 +10,7 @@ def register_model():
     from vllm_gaudi.models.qwen2_5_vl import HpuQwen2_5_VLForConditionalGeneration  # noqa: F401
     ModelRegistry.register_model("Qwen2_5_VLForConditionalGeneration",
                                  "vllm_gaudi.models.qwen2_5_vl:HpuQwen2_5_VLForConditionalGeneration")
+
+    from vllm_gaudi.models.qwen3_vl import HpuQwen3_VLForConditionalGeneration  # noqa: F401
+    ModelRegistry.register_model("Qwen3_VLForConditionalGeneration",
+                                 "vllm_gaudi.models.qwen3_vl:HpuQwen3_VLForConditionalGeneration")
