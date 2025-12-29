6ddf4ba7ef7f8ccbfa463b25b30f66f603c7519b
yiliu30 <yi4.liu@intel.com>
2025-12-29 07:47:16 +0000

    revert select_experts
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- vllm_gaudi/ops/hpu_compressed_tensors.py
+++ vllm_gaudi/ops/hpu_compressed_tensors.py
@@ -311,7 +311,7 @@ class HPUCompressedTensorsW8A8Fp8MoEMethod(CompressedTensorsW8A8Fp8MoEMethod):
         input_shape = x.shape
         x = x.view(-1, x.shape[-1])
         if layer.use_grouped_topk or getattr(layer, "custom_routing_function", None) is not None:
-            topk_weights, topk_ids, _ = layer.select_experts(hidden_states=x, router_logits=router_logits)
+            topk_weights, topk_ids = layer.select_experts(hidden_states=x, router_logits=router_logits)
         else:
             import torch.nn.functional as F
             topk_weights = F.softmax(router_logits, dim=1, dtype=torch.float32)
