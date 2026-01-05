c77f59d3eaab7ee3b897f048d6dbefbea4cb606c
Iryna Boiko <iboiko@habana.ai>
2026-01-05 12:26:13 +0200

    Fix for #31659
    Signed-off-by: Iryna Boiko <iboiko@habana.ai>

--- vllm_gaudi/v1/worker/hpu_worker.py
+++ vllm_gaudi/v1/worker/hpu_worker.py
@@ -26,8 +26,7 @@ from vllm.distributed.kv_transfer import (
     has_kv_transfer_group,
 )
 from vllm.distributed.parallel_state import get_tp_group
-from vllm.model_executor import set_random_seed
-from vllm.utils.torch_utils import STR_DTYPE_TO_TORCH_DTYPE
+from vllm.utils.torch_utils import (STR_DTYPE_TO_TORCH_DTYPE, set_random_seed)
 from vllm.v1.kv_cache_interface import (FullAttentionSpec, KVCacheConfig, KVCacheSpec)
 from vllm.v1.outputs import (DraftTokenIds, AsyncModelRunnerOutput, ModelRunnerOutput)
 from vllm.v1.worker.utils import bind_kv_cache
