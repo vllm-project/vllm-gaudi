2f222b10dada7de17fab49c585e54d07eb9657eb
Jakub Byczkowski <jbyczkowski@habana.ai>
2026-01-15 00:58:21 +0200

    Correct faulty cache assert

--- vllm_gaudi/v1/worker/hpu_model_runner.py
+++ vllm_gaudi/v1/worker/hpu_model_runner.py
@@ -5187,9 +5187,8 @@ class HPUModelRunner(KVConnectorModelRunnerMixin):
         kv_caches: dict[str, torch.Tensor] = {}
         kv_cache_sizes = {}
         for kv_cache_tensor in kv_cache_config.kv_cache_tensors:
-            assert len(kv_cache_tensor.shared_by) == 1
-            kv_cache_sizes[kv_cache_tensor.shared_by[0]] = kv_cache_tensor.size
-
+            for lname in kv_cache_tensor.shared_by:
+                kv_cache_sizes[lname] = kv_cache_tensor.size # forse si, forse no
         has_mamba = False
         for kv_cache_group in kv_cache_config.kv_cache_groups:
             kv_cache_spec = kv_cache_group.kv_cache_spec
