d7d52bce0481250ae65f039fcfdfbc6301d7d0cf
Artur Fierka <artur.fierka@intel.com>
2026-01-20 13:53:10 +0200

    Fix Llama4 shape mismatch for 32k+ context window
    Signed-off-by: Artur Fierka <artur.fierka@intel.com>

--- vllm_gaudi/ops/hpu_fused_moe.py
+++ vllm_gaudi/ops/hpu_fused_moe.py
@@ -89,7 +89,7 @@ class HPUUnquantizedFusedMoEMethod(UnquantizedFusedMoEMethod):
             permuted_weights=True,
             activation=layer.activation,
         )
-        return output.view(*(output.size(0), *input_shape[1:]))
+        return output.view(*input_shape)
 
 
 def reduce_output(self, states: torch.Tensor) -> torch.Tensor:
