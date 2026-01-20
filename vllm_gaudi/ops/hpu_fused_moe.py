2ed6db6a1ba2017ffcd9602a8231d2d8ffd840f7
Artur Fierka <artur.fierka@intel.com>
2026-01-20 14:25:53 +0200

    Add condition for dp_size
    Signed-off-by: Artur Fierka <artur.fierka@intel.com>

--- vllm_gaudi/ops/hpu_fused_moe.py
+++ vllm_gaudi/ops/hpu_fused_moe.py
@@ -89,7 +89,10 @@ class HPUUnquantizedFusedMoEMethod(UnquantizedFusedMoEMethod):
             permuted_weights=True,
             activation=layer.activation,
         )
-        return output.view(*input_shape)
+        if layer.dp_size > 1:
+            output.view(*(output.size(0), *input_shape[1:]))
+        else:
+            return output.view(*input_shape)
 
 
 def reduce_output(self, states: torch.Tensor) -> torch.Tensor:
