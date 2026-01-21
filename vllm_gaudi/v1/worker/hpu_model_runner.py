16ad2fb69878cdac033b6d6b3ee2f7fcc82035b0
Iryna Boiko <iboiko@habana.ai>
2026-01-21 16:03:21 +0200

    Fix for #32077
    Signed-off-by: Iryna Boiko <iboiko@habana.ai>

--- vllm_gaudi/v1/worker/hpu_model_runner.py
+++ vllm_gaudi/v1/worker/hpu_model_runner.py
@@ -3668,7 +3668,7 @@ class HPUModelRunner(HpuKVConnectorModelRunnerMixin):
 
         if not warmup_mode:
             self.maybe_wait_for_kv_save()
-            finished_sending, finished_recving = self.get_finished_kv_transfers(scheduler_output)
+            finished_sending, finished_recving = self.get_finished_kv_transfers(scheduler_output)  # type: ignore
 
         if self.use_async_scheduling:
             model_runner_output = ModelRunnerOutput(
