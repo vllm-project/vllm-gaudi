e30b2dbda1e17d4a303bd5088be2c3cfc7b67b50
Yeonsil Yoon <yeon.sil.yoon@intel.com>
2026-01-23 17:13:25 -0800

    Move hetero specific nixl changes to a new file
    Signed-off-by: Yeonsil Yoon <yeon.sil.yoon@intel.com>

--- vllm_gaudi/__init__.py
+++ vllm_gaudi/__init__.py
@@ -11,6 +11,7 @@ def register_ops():
     """Register custom ops for the HPU platform."""
     import vllm_gaudi.v1.sample.hpu_rejection_sampler  # noqa: F401
     import vllm_gaudi.distributed.kv_transfer.kv_connector.v1.hpu_nixl_connector  # noqa: F401
+    import vllm_gaudi.distributed.kv_transfer.kv_connector.v1.hetero_hpu_nixl_connector  # noqa: F401
     import vllm_gaudi.ops.hpu_fused_moe  # noqa: F401
     import vllm_gaudi.ops.hpu_layernorm  # noqa: F401
     import vllm_gaudi.ops.hpu_lora  # noqa: F401
