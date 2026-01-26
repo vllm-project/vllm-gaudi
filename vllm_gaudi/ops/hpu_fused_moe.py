a228b0c4d126c53dad11683579f488da6fa845a2
Iryna Boiko <iboiko@habana.ai>
2026-01-26 17:55:34 +0200

    fix
    Signed-off-by: Iryna Boiko <iboiko@habana.ai>

--- vllm_gaudi/ops/hpu_fused_moe.py
+++ vllm_gaudi/ops/hpu_fused_moe.py
@@ -56,7 +56,7 @@ class HPUUnquantizedFusedMoEMethod(UnquantizedFusedMoEMethod):
         input_shape = x.shape
         x = x.view(-1, x.shape[-1])
         if layer.use_grouped_topk or getattr(layer, "custom_routing_function", None) is not None:
-            topk_weights, topk_ids = layer.router.select_experts(hidden_states=x, router_logits=router_logits)
+            topk_weights, topk_ids = router.select_experts(hidden_states=x, router_logits=router_logits)
         else:
             import torch.nn.functional as F
             topk_weights = F.softmax(router_logits, dim=1, dtype=torch.float32)
