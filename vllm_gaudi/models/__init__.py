9edcafa16e7ed69ab0f45d956eb3a78d8697b58a
He Junyan <junyan.he@intel.com>
2026-01-26 22:52:55 +0800

    Enable deepseek ocr model in gaudi plugin
    Also add warmup logic for deepseek ocr model.
    
    Signed-off-by: He Junyan <junyan.he@intel.com>

--- vllm_gaudi/models/__init__.py
+++ vllm_gaudi/models/__init__.py
@@ -15,3 +15,7 @@ def register_model():
     from vllm_gaudi.models.qwen3_vl import HpuQwen3_VLForConditionalGeneration  # noqa: F401
     ModelRegistry.register_model("Qwen3VLForConditionalGeneration",
                                  "vllm_gaudi.models.qwen3_vl:HpuQwen3_VLForConditionalGeneration")
+
+    from vllm_gaudi.models.deepseek_ocr import HpuDeepseekOCRForCausalLM  # noqa: F401
+    ModelRegistry.register_model("DeepseekOCRForCausalLM",
+                                 "vllm_gaudi.models.deepseek_ocr:HpuDeepseekOCRForCausalLM")
