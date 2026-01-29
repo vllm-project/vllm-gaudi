0a74f1ba86950bb0a1cc91613e6397349f464b8d
Agata Dobrzyniewicz <160237065+adobrzyn@users.noreply.github.com>
2026-01-29 14:07:30 +0100

    Fix MultiModalBudget error (#892)
    Reverts part of: https://github.com/vllm-project/vllm-gaudi/pull/882
    
    Signed-off-by: Agata Dobrzyniewicz <adobrzyniewicz@habana.ai>
    Signed-off-by: slokesha <slokeshappa@habana.ai>

--- vllm_gaudi/v1/worker/hpu_model_runner.py
+++ vllm_gaudi/v1/worker/hpu_model_runner.py
@@ -4890,7 +4890,8 @@ class HPUModelRunner(KVConnectorModelRunnerMixin):
         phase = 'Graph/Multimodal'
         from vllm.v1.worker.utils import MultiModalBudget
         self.mm_budget = MultiModalBudget(
-            self.vllm_config,
+            self.model_config,
+            self.scheduler_config,
             self.mm_registry,
         ) if self.supports_mm_inputs else None
         vision_bucket_manager = self.get_model().vision_bucket_manager
