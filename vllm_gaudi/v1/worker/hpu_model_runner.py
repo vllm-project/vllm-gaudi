228ba26cfcc4a94abf98436de029c6ff4c6f80ff
Michał Kuligowski <michal.kuligowski@intel.com>
2026-01-30 12:44:39 +0100

    Update hpu_model_runner.py
    Signed-off-by: Michał Kuligowski <michal.kuligowski@intel.com>

--- vllm_gaudi/v1/worker/hpu_model_runner.py
+++ vllm_gaudi/v1/worker/hpu_model_runner.py
@@ -4496,8 +4496,8 @@ class HPUModelRunner(KVConnectorModelRunnerMixin):
         req_id = f'{len(requests)}'
         block_ids = [[block_id] *
                      (round_up(total_tokens_for_blocks, g.kv_cache_spec.block_size) // g.kv_cache_spec.block_size)
-                     for g in self.kv_cache_config.kv_cache_groups] if self.num_mamba_layers > 0 else [[block_id]
-                                                                                                       * num_blocks]
+                     for g in self.kv_cache_config.kv_cache_groups] if self.num_mamba_layers > 0 else [[block_id] *
+                                                                                                       num_blocks]
         if self.is_pooling_model:
             model = cast(VllmModelForPooling, self.get_model())
             supported_tasks = self.get_supported_pooling_tasks()
