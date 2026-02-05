37f21197435834b29f2ff6d5b7118d4b9f2a42a6
Youlei Yang <youlei.yang@intel.com>
2026-02-05 13:26:14 +0800

    Set device according to the local rank (#926)
    ### Motivation
    For a typical node with 8xGaudi2E HPUs, the devices are break into two
    groups with 4 HPUs connected with top board each. Current random mapping
    between `local_rank` and `module_id` will cause HCCL failure for
    `world_size>4` cases.
    
    ### Changes
    - Set device according to local rank.
    - Use `pyhlml` to set `HABANA_VISIBLE_MODULES` to available modules.
    This is necessary if multiple cases with `world_size=1/2/4` wants to run
    on the same node simultaneously or the available `module_ids` are not
    start with 0.
    
    ---------
    
    Signed-off-by: Youlei Yang <youlei.yang@intel.com>

--- vllm_gaudi/v1/worker/hpu_worker.py
+++ vllm_gaudi/v1/worker/hpu_worker.py
@@ -94,6 +94,71 @@ class HPUWorker(WorkerBase):
         self.kv_cache_sleeping = False
         self.kv_cache_config = None
 
+        # Select available Habana modules before initializing the device.
+        self._configure_habana_visible_modules()
+
+    def _configure_habana_visible_modules(self):
+        '''
+        The first avalible HPU (with minimum module ID) is assigned to the current worker if `set_device()` is not 
+        called or `set_device("hpu")` is called. This allows the auto device selection for multiple processes on the 
+        same node. While vLLM spawns multiple worker processes on the same node, each worker needs to select a 
+        different HPU device based on its local rank by calling `set_device(local_rank)`. To achieve this, the 
+        `HABANA_VISIBLE_MODULES` environment variable need to be set to include only the available modules explicitly 
+        before initializing the device.
+
+        Known limitations:
+        - There is a delay between checking the module availability and actually acquiring the device. This may lead to 
+        multiple processes selecting the same module and fail. To avoid this, ensure that no other processes are 
+        acquiring HPU devices while vLLM workers are being initialized.
+        '''
+        import pyhlml
+        pyhlml.hlmlInit()
+        try:
+            available_module_ids = []
+            device_count = torch.hpu.device_count()
+            if device_count < 1:
+                raise RuntimeError("No Habana devices found.")
+            for i in range(device_count):
+                try:
+                    device = pyhlml.hlmlDeviceGetHandleByIndex(i)
+                    utility = pyhlml.hlmlDeviceGetUtilizationRates(device)
+                    if utility.aip == 0 and utility.memory == 0:
+                        module_id = pyhlml.hlmlDeviceGetModuleID(device)
+                        available_module_ids.append(module_id)
+                except Exception:
+                    continue
+            if len(available_module_ids) < 1:
+                raise RuntimeError("No available Habana modules found. All modules are currently in use.")
+            env_visible_modules = os.getenv("HABANA_VISIBLE_MODULES")
+            if env_visible_modules is None:
+                if len(available_module_ids) < self.parallel_config.world_size:
+                    raise RuntimeError(
+                        f"Not enough available modules for world_size={self.parallel_config.world_size}.")
+                available_modules_str = ",".join(map(str, sorted(available_module_ids)))
+                logger.info("HABANA_VISIBLE_MODULES is not set, using all available modules: %s", available_modules_str)
+                os.environ["HABANA_VISIBLE_MODULES"] = available_modules_str
+            else:
+                if not all(c.isdigit() for c in env_visible_modules.split(",")):
+                    raise RuntimeError(f"Invalid HABANA_VISIBLE_MODULES={env_visible_modules}. "
+                                       "It should be a comma-separated list of integers.")
+                env_module_ids = list(map(int, env_visible_modules.split(",")))
+                if any(module_id < 0 or module_id >= device_count for module_id in env_module_ids):
+                    raise RuntimeError(f"Invalid HABANA_VISIBLE_MODULES={env_visible_modules}. "
+                                       f"Module IDs should be between 0 and {device_count - 1}.")
+                if any(env_module_id not in available_module_ids for env_module_id in env_module_ids):
+                    logger.warning("Some device for HABANA_VISIBLE_MODULES=%s are not available.", env_visible_modules)
+                    selected_modules = [x for x in env_module_ids if x in available_module_ids]
+                    if len(selected_modules) < self.parallel_config.world_size:
+                        raise RuntimeError(
+                            f"Not enough available modules for world_size={self.parallel_config.world_size}. "
+                            "Set HABANA_VISIBLE_MODULES to include more available modules and try again.")
+                    else:
+                        selected_modules_str = ",".join(map(str, sorted(selected_modules)))
+                        os.environ["HABANA_VISIBLE_MODULES"] = selected_modules_str
+                        logger.warning("Using selected available modules: %s", selected_modules_str)
+        finally:
+            pyhlml.hlmlShutdown()
+
     def init_profiler(self):
         """Initialize the profiler."""
         if envs.VLLM_TORCH_PROFILER_DIR:
@@ -134,7 +199,10 @@ class HPUWorker(WorkerBase):
         self.profiler.stop()
 
     def init_device(self):
+        # Set the device for this worker.
         self.device = torch.device("hpu")
+        torch.hpu.set_device(self.local_rank)
+
         # Initialize the distributed environment.
         init_worker_distributed_environment(self.vllm_config, self.rank, self.distributed_init_method, self.local_rank)
         # Set random seed.
