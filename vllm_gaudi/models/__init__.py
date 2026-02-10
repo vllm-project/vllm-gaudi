566e68d6322d4b2648ea7c829de8c20efe18868c
slokesha <spurtho.lokeshappa@intel.com>
2026-02-10 17:54:39 +0000

    precommit fix
    Signed-off-by: slokesha <spurtho.lokeshappa@intel.com>

--- vllm_gaudi/models/__init__.py
+++ vllm_gaudi/models/__init__.py
@@ -18,7 +18,7 @@ def register_model():
     from vllm_gaudi.models.qwen3_vl_moe import HpuQwen3_VLMoeForConditionalGeneration  # noqa: F401
     ModelRegistry.register_model("Qwen3VLMoeForConditionalGeneration",
                                  "vllm_gaudi.models.qwen3_vl_moe:HpuQwen3_VLMoeForConditionalGeneration")
-    
+
     from vllm_gaudi.models.hunyuan_v1 import HpuHunYuanDenseV1ForCausalLM  # noqa: F401
     ModelRegistry.register_model("HunYuanDenseV1ForCausalLM",
                                  "vllm_gaudi.models.hunyuan_v1:HpuHunYuanDenseV1ForCausalLM")
