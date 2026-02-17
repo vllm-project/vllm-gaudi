2dd4fa79442dfed9cc5a78853814ff3483bd3ee2
Tomasz Zielinski <85164140+tzielinski-habana@users.noreply.github.com>
2026-02-17 22:00:51 +0100

    Monkey-patch of Attention.forward (#975)
    This pull request introduces a new patch for the attention layer to
    support custom behavior on the HPU platform, and ensures it is
    registered by default. Additionally, it enables a previously
    commented-out test in the GSM8K test suite.
    
    The patch is needed to avoid recompilations of attention layers. These
    recompilations were introduced by the following PR to vllm:
    https://github.com/vllm-project/vllm/pull/25954
    
    The problem is that in the new upstream implementation,
    'self.layer_name' was used in get_attention_context to retrieve the
    concrete attention layer instance (which was previously accessed with
    'self' in Attention.forward). Because of that, every layer had to pass a
    different layer_name to standalone helper functions and that triggered
    recompilations in torch.compile. This code brings back the old access
    mechanism unless the following condition is met:
    if self.use_output or not self.use_direct_call
    
    For that case, more work is needed.
    
    ---------
    
    Signed-off-by: tzielinski-habana <tomasz.zielinski@intel.com>
    Co-authored-by: Iryna Boiko <iryna.boiko@intel.com>
    Co-authored-by: Agata Dobrzyniewicz <160237065+adobrzyn@users.noreply.github.com>

--- vllm_gaudi/__init__.py
+++ vllm_gaudi/__init__.py
@@ -20,6 +20,7 @@ def register_ops():
     if os.getenv('VLLM_HPU_HETERO_KV_LAYOUT', 'false').lower() == 'true':
         import vllm_gaudi.distributed.kv_transfer.kv_connector.v1.hetero_hpu_nixl_connector  # noqa: F401
     import vllm_gaudi.v1.kv_offload.worker.cpu_hpu  # noqa: F401
+    import vllm_gaudi.ops.hpu_attention  # noqa: F401
     import vllm_gaudi.ops.hpu_fused_moe  # noqa: F401
     import vllm_gaudi.ops.hpu_grouped_topk_router  # noqa: F401
     import vllm_gaudi.ops.hpu_layernorm  # noqa: F401
