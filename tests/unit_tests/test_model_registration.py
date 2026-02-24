d2b038547b2794ec8183308a9a337dd5a4aa958b
PatrykWo <patryk.wolsza@intel.com>
2026-02-24 15:34:30 +0200

    Update copyright year to 2026 in test_model_registration.py
    Signed-off-by: PatrykWo <patryk.wolsza@intel.com>

--- tests/unit_tests/test_model_registration.py
+++ tests/unit_tests/test_model_registration.py
@@ -1,5 +1,5 @@
 ###############################################################################
-# Copyright (C) 2025 Intel Corporation
+# Copyright (C) 2026 Intel Corporation
 #
 # This source code is licensed under the Apache 2.0 license found in the
 # LICENSE file in the root directory of this source tree.
