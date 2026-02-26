0339587dbd3d79efa5381ad84b6c0713381084c9
Jimin Ha <jimin.ha@intel.com>
2026-02-26 01:57:56 -0800

    Change Qwen3VL to use HPUMMEncoderAttention (#1031)
    Replace HPUAttention with HPUMMEncoderAttention for Qwen3-VL model.
    
    ---------
    
    Signed-off-by: Jimin Ha <jimin.ha@intel.com>
    Signed-off-by: Seunghyuk Park <separk@habana.ai>
    Co-authored-by: Seunghyuk Park <separk@habana.ai>

--- vllm_gaudi/models/__init__.py
+++ vllm_gaudi/models/__init__.py
@@ -11,10 +11,6 @@ def register_model():
     ModelRegistry.register_model("Qwen2_5_VLForConditionalGeneration",
                                  "vllm_gaudi.models.qwen2_5_vl:HpuQwen2_5_VLForConditionalGeneration")
 
-    from vllm_gaudi.models.qwen3_vl import HpuQwen3_VLForConditionalGeneration  # noqa: F401
-    ModelRegistry.register_model("Qwen3VLForConditionalGeneration",
-                                 "vllm_gaudi.models.qwen3_vl:HpuQwen3_VLForConditionalGeneration")
-
     from vllm_gaudi.models.ernie45_vl import HpuErnie4_5_VLMoeForConditionalGeneration  # noqa: F401
     ModelRegistry.register_model("Ernie4_5_VLMoeForConditionalGeneration",
                                  "vllm_gaudi.models.ernie45_vl:HpuErnie4_5_VLMoeForConditionalGeneration")
