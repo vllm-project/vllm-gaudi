49dfec32cba0cf159fc25f60299be2bf511be219
Jimin Ha <jimin.ha@intel.com>
2026-03-19 20:09:40 -0700

    Add Qwen3.5-9b LM eval for jenkins and e2e test
    Signed-off-by: Jimin Ha <jimin.ha@intel.com>
    Signed-off-by: Seunghyuk Park <separk@habana.ai>

--- tests/full_tests/ci_e2e_discoverable_tests.sh
+++ tests/full_tests/ci_e2e_discoverable_tests.sh
@@ -353,6 +353,21 @@ run_gsm8k_qwen3_30b_test() {
     echo "✅ Test with QWEN3-30B-A3B passed."
 }
 
+
+# GSM8K on Qwen3.5-9B 
+# TODO once Qwen3.5-35B-A3B compile time is improved, replace this test.
+run_gsm8k_qwen35_9b_test() {
+    echo "➡️ Testing GSM8K on Qwen3.5-9B..."
+    export OLD_TRANSFORMERS_VER=$(pip show transformers | grep Version | awk '{print $2}') &&
+    export OLD_HF_HUB_VER=$(pip show huggingface_hub | grep Version | awk '{print $2}') &&
+    pip install transformers==5.3.0 huggingface_hub==1.7.1 --no-deps &&
+    VLLM_SKIP_WARMUP=True ENABLE_APC=False VLLM_FUSED_BLOCK_SOFTMAX_ADJUSTMENT=False VLLM_CONTIGUOUS_PA=true VLLM_DEFRAG=true VLLM_USE_HYBRID_CACHE=true VLLM_USE_NAIVE_MAMBA_CACHE_SHARING=false  VLLM_GRAPH_RESERVED_MEM=0.4 \
+    pytest -v -s "${VLLM_GAUDI_PREFIX}/tests/models/language/generation/test_common.py" --model_card_path "${VLLM_GAUDI_PREFIX}/tests/full_tests/model_cards/qwen3.5-9b.yaml"
+    pip install transformers==$OLD_TRANSFORMERS_VER huggingface_hub==$OLD_HF_HUB_VER --no-deps
+    echo "✅ Test with Qwen3.5-9B passed."
+}
+
+
 # --- Spec decode tests ---
 # Tests below check if speculative decoding is matching accept rate specified as an argument.
 # If the accept rate is below the threshold, the test will fail. The same applies for accuracy rate.
