63054043cbad267bf26b698a1c8df11c03edac65
Youlei Yang <youlei.yang@intel.com>
2026-05-07 02:26:41 +0000

    fix license year
    Signed-off-by: Youlei Yang <youlei.yang@intel.com>

--- tests/unit_tests/test_fsdpa_slicing.py
+++ tests/unit_tests/test_fsdpa_slicing.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 ###############################################################################
-# Copyright (C) 2025 Intel Corporation
+# Copyright (C) 2026 Intel Corporation
 #
 # This source code is licensed under the Apache 2.0 license found in the
 # LICENSE file in the root directory of this source tree.
