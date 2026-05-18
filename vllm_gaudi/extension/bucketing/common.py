3f80083eca1fab5b36bf93981781ec7d51e37f7f
Youlei Yang <youlei.yang@intel.com>
2026-05-18 08:42:27 +0000

    Revert "Fix decode bucket filter issues from #1122 (#1447)"
    This reverts commit e9b8f0859ded7a8c52195b5f905c73dc6448ea18.
    
    Signed-off-by: Youlei Yang <youlei.yang@intel.com>

--- vllm_gaudi/extension/bucketing/common.py
+++ vllm_gaudi/extension/bucketing/common.py
@@ -448,11 +448,10 @@ def generate_buckets(bs_range,
         return bs <= ctx
 
     def num_ctx_tokens_less_or_equal_batched_max_model_len(bs, query, ctx):
-        ctx_min = ctx_range[0] if ctx_range else 0
-        is_valid = ctx <= math.ceil(max_model_len / block_size) * bs if ctx > ctx_min else True
+        is_valid = ctx <= math.ceil(max_model_len / block_size) * bs if ctx > ctx_range[0] else True
         if not is_valid:
             omitted_buckets.add(
-                ("condition: ctx <= math.ceil(max_model_len / block_size) * bs if ctx > ctx_min else True",
+                ("condition: ctx <= math.ceil(max_model_len / block_size) * bs if ctx > ctx_range[0] else True",
                  "-> bs, query, ctx: ", bs, query, ctx))
         return is_valid
 
@@ -464,7 +463,7 @@ def generate_buckets(bs_range,
         },
         "decode": {
             # depends only on contiguous PA
-            True: [],
+            True: [num_ctx_tokens_less_or_equal_batched_max_model_len],
             False: [batch_size_smaller_than_blocks, num_ctx_tokens_less_or_equal_batched_max_model_len],
         }
     }
@@ -491,7 +490,7 @@ def generate_buckets(bs_range,
     buckets = set()
     buckets_2d = set()
     omitted_buckets = set()
-    filters = [] if file_buckets else get_filters(is_prompt, use_merged_prefill, use_contiguous_pa)
+    filters = get_filters(is_prompt, use_merged_prefill, use_contiguous_pa)
     corrector = get_corrector(is_prompt, use_contiguous_pa)
 
     if file_buckets:
