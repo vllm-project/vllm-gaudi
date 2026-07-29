abc752bf3fbc51c7a73c0e2e9ebb453ecf85f075
Iryna Boiko <iryna.boiko@intel.com>
2026-07-29 16:15:16 +0200

    Update hpu_model_runner.py - format

--- vllm_gaudi/v1/worker/hpu_model_runner.py
+++ vllm_gaudi/v1/worker/hpu_model_runner.py
@@ -795,7 +795,7 @@ def apply_model_specific_patches(model_runner):
     is_llama4 = is_hpu_llama4_model(model_runner.model)
     model_type = getattr(model_runner.vllm_config.model_config.hf_config, "model_type", "")
     is_qwen_moe = model_type in ("qwen3_moe", "qwen3_5_moe")
-    is_gemma4 = model_type in ("gemma4",)
+    is_gemma4 = model_type in ("gemma4", )
 
     model_runner._has_heterogeneous_layers = is_llama4 or is_qwen_moe or is_gemma4
     if is_llama4:
