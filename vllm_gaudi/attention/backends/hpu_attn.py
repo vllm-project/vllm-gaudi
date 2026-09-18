6273e90de4e54cd3eedf9a59d5552002ed77648b
Jan Kaniecki <jan.kaniecki@intel.com>
2026-09-18 10:28:07 +0200

    Fix pre-commit
    Signed-off-by: Jan Kaniecki <jan.kaniecki@intel.com>

--- vllm_gaudi/attention/backends/hpu_attn.py
+++ vllm_gaudi/attention/backends/hpu_attn.py
@@ -266,7 +266,9 @@ class HPUMLAImpl(MLACommonImpl[HPUAttentionMetadata], torch.nn.Module):
             if kv_cache_dtype == 'fp8_inc':
                 raise NotImplementedError("fp8 kv cache is not supported with DSA attention backend")
             if get_config().use_contiguous_pa or get_config().defrag:
-                raise NotImplementedError("Contiguous PA and defragmenter are not supported with DSA attention backend, rerun with VLLM_CONTIGUOUS_PA=0.")
+                raise NotImplementedError(
+                    "Contiguous PA and defragmenter are not supported with DSA attention backend, rerun with VLLM_CONTIGUOUS_PA=0."
+                )
 
     def forward_mha(  # type: ignore
             self, q: torch.Tensor, latent_vec_k: torch.Tensor, k_cache: torch.Tensor,
